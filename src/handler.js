60bf8a10f78d07a436e021d1be22291e2f7bfe56
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-10 17:37:22 +0700

    Add: method addBookHandler

---
+++ src/handler.js
@@ -0,0 +1,80 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+	const {
+		name,
+		year,
+		author,
+		summary,
+		publisher,
+		pageCount,
+		readPage,
+		reading, } = request.payload;
+	
+	const id = nanoid(16);
+	const insertedAt = new Date().toISOString();
+	const updatedAt = insertedAt;
+	let finished = false;
+	
+	if (pageCount === readPage) {
+		finished = true;
+	}
+	
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+	};
+	
+	if (!name) {
+		const response = h.response({
+			status: 'fail',
+			message: 'Gagal menambahkan buku. Mohon isi nama buku'
+		});
+		response.code(400);
+		return response;
+	}
+	
+	if (readPage > pageCount) {
+		const response = h.response({
+			status: 'fail',
+			message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+		})
+		response.code(400);
+		return response;
+	}
+	
+	const response = h.response({
+		status: 'error',
+		message: 'Buku gagal ditambahkan'
+	});
+	books.push(newBook);
+	const isSuccess = books.filter((book) => book.id === id).length > 0;
+	if (isSuccess) {
+		const response = h.response({
+			status: 'success',
+			message: 'Buku berhasil ditambahkan',
+			data: {
+				bookId: id
+			},
+		});
+		response.code(201);
+		return response;
+	}
+	response.code(500);
+	return response;
+};
+
+module.exports = {
+	addBookHandler,
+}
