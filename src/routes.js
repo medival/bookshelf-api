57650f64143bc7a65d76dc4fe396c45a5a2c3bca
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-10 15:59:23 +0700

    Create route

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+	{
+		method: 'POST',
+		path: '/books',
+		handler: (request, h) => {}
+	},
+	{
+		method: 'GET',
+		path: '/books',
+		handler: (request, h) => {}
+	},
+	{
+		method: 'GET',
+		path: '/books/{bookId}',
+		handler: (request, h) => {}
+	},
+	{
+		method: 'PUT',
+		path: '/books/{bookId}',
+		handler: (request, h) => {}
+	},
+	{
+		method: 'DELETE',
+		path: '/books/{bookId}',
+		handler: (request, h) => {}
+	}
+]
+
+module.exports = routes;
