825d8bf4041e633d7af532976ee55d248cc458e8
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-10 21:26:58 +0700

    Create: editBookById

--- src/handler.js
+++ src/handler.js
@@ -167,6 +167,65 @@ const getBooksByIdHandler = (request, h) => {
 }
 
 const editBookByIdHandler = (request, h) => {
+	const { bookId } = request.params;
+	const {
+		name,
+		year,
+		author,
+		summary,
+		publisher,
+		pageCount,
+		readPage,
+		reading
+	} = request.payload;
+	
+	if (name === undefined) {
+		const response = h.response({
+			status: 'fail',
+			message: 'Gagal memperbarui buku. Mohon isi nama buku'
+		});
+		response.code(400);
+		return response;
+	}
+	
+	if (readPage > pageCount) {
+		const response = h.response({
+			status: 'fail',
+			message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+		});
+		response.code(400);
+		return response;
+	}
+	
+	const index = books.findIndex((book) => book.id === bookId);
+	if (index === -1) {
+		const response = h.response({
+			status: 'fail',
+			message: 'Gagal memperbarui buku. Id tidak ditemukan'
+		});
+		response.code(404);
+		return response;
+	}
+	
+	books[index] = {
+		...books[index],
+		name,
+		year,
+		author,
+		summary,
+		publisher,
+		pageCount,
+		readPage,
+		reading,
+		finished: pageCount === readPage,
+		updatedAt: new Date().toString(),
+	};
+	const response = h.response({
+		status: 'success',
+		message: 'Buku berhasil diperbarui'
+	});
+	response.code(200);
+	return response;
 	
 };
 
