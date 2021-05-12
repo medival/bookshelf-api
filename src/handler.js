4b2e90f93d395099db36f2fef8bd0ce74ee0d946
Adi  Purnomo <dev.adipurnomo@gmail.com>
2021-05-12 17:39:05 +0700

    Fix: deleteBookById

--- src/handler.js
+++ src/handler.js
@@ -232,21 +232,19 @@ const editBookByIdHandler = (request, h) => {
 
 const deleteBookById = (request, h) => {
   const { bookId } = request.params;
-
   const index = books.findIndex((book) => book.id === bookId);
 
   if (index !== -1) {
     books.splice(index, 1);
     return h.response({
-      status: 'fail',
-      message: 'Buku gagal dihapus. Id tidak ditemukan',
-
-    }).code(404);
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    }).code(200);
   }
 
   return h.response({
-    status: 'success',
-    message: 'Buku berhasil dihapus',
+    status: 'fail',
+     message: 'Buku gagal dihapus. Id tidak ditemukan',
   }).code(200);
 };
 
