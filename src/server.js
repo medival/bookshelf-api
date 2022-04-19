0d77a85fb3da60a728fd30ba10b3a988825407b4
Restyled.io <commits@restyled.io>
2022-04-19 06:16:06 +0000

    Restyled by prettier

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
-    port : 5000,
-    host : '${{ secrets.HOST }}',
-    routes : {
-      cors : {
-        origin : [ '*' ],
+    port: 5000,
+    host: "${{ secrets.HOST }}",
+    routes: {
+      cors: {
+        origin: ["*"],
       },
     },
   });
